b4573aa516b363cf0ed3e8830c23cd31db5df2c7
Nathan Castaldi <nathan@castaldi.dev>
2021-02-13 07:40:51 -0500

    adjust api call

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=20";
+const BASEURL = "https://randomuser.me/api/?results=5&nat=us";
 
 export default {
     search: function() {
