5a2f3c56cde34e29d84aaea9eeb4000c30124d6c
Nathan Castaldi <nathan@castaldi.dev>
2021-02-13 13:26:18 -0500

    added input and button

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -2,25 +2,28 @@ import React from "react";
 
 const Search = (props) => {
   return (
-    <form>
-      <div className="form-group">
-        <input
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search for an employee"
-          id="search"
-        />
+    <div class="input-group mb-3">
+      <input
+        onChange={props.handleInputChange}
+        value={props.search}
+        name="search"
+        type="text"
+        className="form-control"
+        placeholder="Search for an employee"
+        id="search"
+        aria-label="Recipient's username"
+        aria-describedby="basic-addon2"
+      />
+      <div class="input-group-append">
         <button
           onClick={props.handleFormSubmit}
-          className="btn btn-primary mt-3"
+          class="btn btn-outline-secondary"
+          type="button"
         >
           Search
         </button>
       </div>
-    </form>
+    </div>
   );
 };
 
