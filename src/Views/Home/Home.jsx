611a7ae8677b56ed2bba7617ff337f2a7de5b235
Nathan Castaldi <nathan@castaldi.dev>
2021-02-13 09:24:16 -0500

    added subheading

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -23,6 +23,7 @@ class Home extends Component {
           <div className="row">
             <div className="col text-center">
               <h1>Employee Directory</h1>
+              <h5>Click 'Name' head to sort or use Search box to filter results</h5>
             </div>
           </div>
           <div className="row">
