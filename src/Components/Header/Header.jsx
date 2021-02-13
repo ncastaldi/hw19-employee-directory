2e8e26f439205630e4166dbff47c1ca71743a071
Nathan Castaldi <nathan@castaldi.dev>
2021-02-13 10:13:57 -0500

    commented out unused heading for clarity

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="col text-center">
       <h1>Employee Directory</h1>
-      <h5>Click 'Name' head to sort or use Search box to filter results</h5>
+      {/* <h5>Click 'Name' head to sort or use Search box to filter results</h5> */}
     </div>
   );
 };
