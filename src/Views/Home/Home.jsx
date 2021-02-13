dcdc5c4caecfca095fa7ccc40982cf63b34cf0f9
Nathan Castaldi <nathan@castaldi.dev>
2021-02-13 09:22:30 -0500

    added office phone col to table

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -31,7 +31,8 @@ class Home extends Component {
                 <tr>
                   <th scope="col">Profile Picture</th>
                   <th scope="col">Name</th>
-                  <th scope="col">Phone</th>
+                  <th scope="col">Office Phone</th>
+                  <th scope="col">Cell Phone</th>
                   <th scope="col">Email</th>
                   <th scope="col">Birthday</th>
                 </tr>
@@ -41,6 +42,7 @@ class Home extends Component {
                       <tr key={employee.id.value}>
                           <td><img src={employee.picture.thumbnail} alt={employee.login.uuid}></img></td>
                           <td>{employee.name.first + " " + employee.name.last}</td>
+                          <td>{employee.phone}</td>
                           <td>{employee.cell}</td>
                           <td><a href={`mailto:`+employee.email}>{employee.email}</a></td>
                           <td>{employee.dob.date.substring(0, 10)}</td>
