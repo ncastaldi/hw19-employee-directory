3b9dd642dd411e9acaed68e35c756683a859a036
Nathan Castaldi <nathan@castaldi.dev>
2021-02-17 17:53:44 -0500

    removed unneeded code

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,15 +7,7 @@ const Header = () => {
       <div className="row">
         <div className="col text-center">
           <h1>Employee Directory</h1>
-          {/* <h5>Click 'Name' head to sort or use Search box to filter results</h5> */}
         </div>
-      </div>
-      <div className="row">
-        <div className="col-3"></div>
-        <div className="col-6">
-          {/* <Search /> */}
-        </div>
-        <div className="col-3"></div>
       </div>
     </>
   );
